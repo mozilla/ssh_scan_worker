5a4257414fbe7b3f2b3d8bc74add2a3de1753adc
Jonathan Claudius <jclaudius@mozilla.com>
2018-06-26 18:17:31 -0400

    Fix quoting

--- build_and_deploy_to_dockerhub.sh
+++ build_and_deploy_to_dockerhub.sh
@@ -3,7 +3,7 @@
 docker build -t mozilla/ssh_scan_worker .
 
 if [[ "$TRAVIS_BRANCH" == "master" ]]; then
-  if [[ "$TRAVIS_PULL_REQUEST" == 'false' ]]; then
+  if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
     docker login -u="$DOCKER_USER" -p="$DOCKER_PASS" ;\
     docker push mozilla/ssh_scan_worker:latest ;\
   else
