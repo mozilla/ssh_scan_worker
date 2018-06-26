b34f8e9278ebb73bc7517aaef90eee5916090137
Jonathan Claudius <jclaudius@mozilla.com>
2018-06-26 18:15:34 -0400

    Add build and deploy script

---
+++ build_and_deploy_to_dockerhub.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+docker build -t mozilla/ssh_scan_worker .
+
+if [[ "$TRAVIS_BRANCH" == "master" ]]; then
+  if [[ "$TRAVIS_PULL_REQUEST" == 'false' ]]; then
+    docker login -u="$DOCKER_USER" -p="$DOCKER_PASS" ;\
+    docker push mozilla/ssh_scan_worker:latest ;\
+  else
+    exit 0
+  fi
+else
+  exit 0
+fi
