ce93c7113e2510e88c9e9b27f0ca05d31e66a8c0
Jonathan Claudius <jclaudius@mozilla.com>
2018-06-27 10:21:02 -0400

    remove space

--- build_and_deploy_to_dockerhub.sh
+++ build_and_deploy_to_dockerhub.sh
@@ -5,7 +5,7 @@ docker build -t mozilla/ssh_scan_worker .
 if [[ "$TRAVIS_BRANCH" == "master" ]]; then
   if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
     echo $DOCKER_PASS | docker login -u="$DOCKER_USER" --password-stdin;\
-    docker push mozilla/ssh_scan_worker ;\
+    docker push mozilla/ssh_scan_worker;\
   else
     exit 0
   fi
